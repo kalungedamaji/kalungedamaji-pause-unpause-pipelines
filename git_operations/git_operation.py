584f9d61d0dc326709b1c01169c56ad864ef46ab
damaji.kalunge <damaji.kalunge@gamesys.co.uk>
2023-08-29 18:06:15 +0530

    added feature to create branch .

---
+++ git_operations/git_operation.py
@@ -0,0 +1,29 @@
+from git import Repo
+def commit_and_push_code(config_repo_path, branch_name, commit_message):
+    print("\n Started committing the changes on branch  \n" + branch_name)
+    PATH_OF_GIT_REPO = config_repo_path+'.git'  # make sure .git folder is properly configured
+
+    repo = Repo(PATH_OF_GIT_REPO)
+
+    # Checkout the main branch
+    main_branch = "main"
+    repo.git.checkout(main_branch)
+
+    # Fetch the latest changes from the remote
+    repo.remotes.origin.fetch()
+
+    # Create  New Branch
+    new_branch = repo.create_head(branch_name)
+    new_branch.checkout()
+
+    # Add and commit the changes
+    repo.index.add('*')
+    repo.index.commit(commit_message)
+
+    # Push the changes to the remote repository
+    origin = repo.remote(name='origin')
+    origin.push(new_branch)
+
+    # Switch back to main branch
+    repo.git.checkout(main_branch)
+    print("\n Pushed changes to the branch " + branch_name)
--- run.py
+++ run.py
@@ -2,17 +2,21 @@ import itertools
 from config_processors.cmt_config import CMTConfig
 from config_processors.gold_config import GoldConfig
 from config_processors.silver_config import SilverConfig
+from git_operations.git_operation import commit_and_push_code
 
 ENVS = ["int", "prod"]
 TERRITORIES = ["na-us-nj", "na-us-pa", "na-ca-on", "eu"]
 
 
-def run_all_silver(config_repo_path, source_system, table_names):
+def run_all_silver(config_repo_path, jira_ticket_no, source_system, table_names):
     print("\n🥈 Updating Silver pipelines\n")
     for env, territory in itertools.product(ENVS, TERRITORIES):
         print("\nFor: ", env, territory, "\n-------------")
         config = SilverConfig(config_repo_path, source_system, env, territory)
         config.remove_table_entries(table_names)
+
+    commit_and_push_code(config_repo_path, jira_ticket_no + "_pause_enrichment_app_for_all_env",
+                         jira_ticket_no + " | Paused table " + ', '.join(map(str, table_names)))
 
 
 def build_all_silver_metadata(config_repo_path, source_system):
@@ -23,12 +27,14 @@ def build_all_silver_metadata(config_repo_path, source_system):
         config.build_metadata()
 
 
-def run_all_gold(config_repo_path, source_system, task_names):
+def run_all_gold(config_repo_path, jira_ticket_no, source_system, task_names):
     print("\n🥇 Updating Gold pipelines\n")
     for env, territory in itertools.product(ENVS, TERRITORIES):
         print("\nFor: ", env, territory, "\n-------------")
         config = GoldConfig(config_repo_path, source_system, env, territory)
         config.remove_tasks_from_workflow(task_names)
+    commit_and_push_code(config_repo_path, jira_ticket_no + "_pause_gold_pipeline_for_all_env",
+                         jira_ticket_no + " | Paused gold task " + ', '.join(map(str, task_names)))
 
 
 def run_all_cmt(config_repo_path, source_system, cmt_version):
@@ -37,23 +43,35 @@ def run_all_cmt(config_repo_path, source_system, cmt_version):
         print("\nFor: ", territory, "\n-------------")
         config = CMTConfig(config_repo_path, source_system, territory)
         config.update_artifact_version(cmt_version)
+    commit_and_push_code(config_repo_path, "update_cmt_version_for_all_env",
+                         "Updated the CMT version to " + cmt_version)
 
 
-def run_all_silver_unpause(config_repo_path, source_system, table_names):
+def run_all_silver_unpause(config_repo_path, jira_ticket_no, source_system, table_names):
     print("\n🥈 Updating Silver pipelines\n")
     for env, territory in itertools.product(ENVS, TERRITORIES):
         print("\nFor: ", env, territory, "\n-------------")
         config = SilverConfig(config_repo_path, source_system, env, territory)
         config.unpause_tables(table_names)
+    commit_and_push_code(config_repo_path, jira_ticket_no + "_un_pause_enrichment_app_for_all_env",
+                         jira_ticket_no + " | un-paused table " + ', '.join(map(str, table_names)))
+
+
+def pause_enrichment_app_for_one_env(config_repo_path, jira_ticket_no, data_source, env, region, table_names):
+    silverConfig = SilverConfig(config_repo_path, data_source, env, region)
+    silverConfig.remove_table_entries(table_names)
+    commit_and_push_code(config_repo_path, jira_ticket_no + "_un_pause_enrichment_app_for_" + region,
+                         jira_ticket_no + " | un-paused table " + ', '.join(map(str, table_names)))
 
 
 # Run for all environments and territories
-# run_all_silver("../vitruvian-deployment-configurations/", "excite", ["comp_type_v1"])
-run_all_silver_unpause(
-    "../vitruvian-deployment-configurations/", "excite", ["comp_type_v1"]
-)
+run_all_silver("../pythonProject/vitruvian-deployment-configurations/", "DSE-10171", "excite",
+               ["command_audit_v1", "high_roller_audit_v1", "campaign_audit_v1"])
+# run_all_silver_unpause(
+#    "../vitruvian-deployment-configurations/", "excite", ["comp_type_v1"]
+# )
 
-# run_all_gold("../vitruvian-deployment-configurations/", "excite", ["fct_compensation_event","fct_payment_event"])
+# run_all_gold("../pythonProject/vitruvian-deployment-configurations/", "excite", ["dim_account_managed_status"])
 # run_all_cmt("../vitruvian-deployment-configurations/", "excite", "1.0.575")
 
 # Build or refresh metadata for all silver tables
@@ -61,9 +79,9 @@ run_all_silver_unpause(
 
 #  Run for specific environment and territory
 
-silverConfig = SilverConfig(
-    "../vitruvian-deployment-configurations/", "excite", "prod", "na-us-nj"
-)
+# silverConfig = SilverConfig(
+#    "../vitruvian-deployment-configurations/", "excite", "prod", "na-us-nj"
+# )
 # silverConfig.remove_table_entries(["comp_type_v1"])
 # silverConfig.build_metadata()
 # silverConfig.unpause_tables(["comp_type_v1"])
